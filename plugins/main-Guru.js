ad317c7ac2ee9af6b9c042d3b4bab7d24dd184c8
frankievon <53036832+frankievon@users.noreply.github.com>
2023-11-07 00:39:35 +0300

    Update main-Guru.js

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -1,33 +1,33 @@
 let handler = async m => m.reply(`
 
-≡  *GURU  ┃ ᴮᴼᵀ*   GROUPS
+≡  Frankie +255629410604  GROUPS
 
 ─────────────
 ▢ Join public bot group and support
-https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
+https://chat.whatsapp.com/KSCZNR8RmZU3queeAa0nxC
 
 ▢ Group 2
-https://chat.whatsapp.com/LwLQab5mZVW6WWRbBPnDGC
+https://chat.whatsapp.com/JLydWTID1PY3j8i81DVZtz
 
 ▢ Group 3
-https://chat.whatsapp.com/Jn9qVerYN6Q1L0vlVFe2XL
+https://chat.whatsapp.com/JLydWTID1PY3j8i81DVZtz
 
 ─────────────
 ≡ Disabled links? enter here! 
 
 ▢ Group WhatsApp 
- https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
+ 
 ─────────────
 ▢ *Owner Telegram*
- https://t.me/i_want_to_be_isekaied
+ 
 
 ▢ *YouTube*
-• https://www.youtube.com/@Asliguru
+• 
 
 
 `.trim())
-handler.help = ['gpguru']
+handler.help = ['gpg']
 handler.tags = ['main']
-handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru'] 
+handler.command = ['groups', '', 'gugp', 'ggp', ''] 
 
 export default handler
